e2dda031315a54591cc0f0b8702f16c98bf3b321
lsj <86811543+Zinisang@users.noreply.github.com>
2021-09-06 13:46:03 +0900

    Initial commit

---
+++ src/main/java/com/example/pinetree/PinetreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinetreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinetreeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pinetree/PinetreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pinetree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinetreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
